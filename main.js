39c72a7c3b7ac7994263a1df36bb9fba33000fbc
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-11-02 15:59:51 -0400

    Initialize dialog

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+
+const addButton = document.querySelector('.add-button');
+const modal = document.querySelector('.modal');
+
+addButton.addEventListener('click', () => {
+  modal.showModal();
+});
+
+modal.addEventListener("click", (e) => {
+  const modalDimensions = modal.getBoundingClientRect()
+  if (
+    e.clientX < modalDimensions.left ||
+    e.clientX > modalDimensions.right ||
+    e.clientY < modalDimensions.top ||
+    e.clientY > modalDimensions.bottom
+  ) {
+    modal.close()
+  }
+})
